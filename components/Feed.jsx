b9ab6e5fa5e4ae5b800049657ec13ca02a148a8e
miillaadddddd <milad7212@gmail.com>
2021-11-13 20:54:33 +0330

    P3

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,22 @@
 function Feed() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <main
+      className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto
+    "
+    >
+      {/* Section */}
+      <section>
+        {/* Stories */}
+        {/* Posts */}
+      </section>
+
+      {/* Section */}
+      <section>
+        {/* Mini Profile */}
+        {/* Suggestion */}
+      </section>
+    </main>
+  );
 }
 
-export default Feed
+export default Feed;
